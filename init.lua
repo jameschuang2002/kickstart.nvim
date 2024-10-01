3158d04e0b794ec1d066ac3016a0eaa716dabef0
James Chuang <jameschuang2002@gmail.com>
2024-10-01 10:25:04 -0700

    nvim tmux navigation

--- init.lua
+++ init.lua
@@ -283,6 +283,22 @@ require('lazy').setup({
     },
   },
   {
+    'alexghergh/nvim-tmux-navigation',
+    config = function()
+      require('nvim-tmux-navigation').setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = '<C-h>',
+          down = '<C-j>',
+          up = '<C-k>',
+          right = '<C-l>',
+          last_active = '<C-\\>',
+          next = '<C-Space>',
+        },
+      }
+    end,
+  },
+  {
     'nvim-tree/nvim-tree.lua',
     keys = {
       { 'n', '<leader>nt', ':NvimTreeToggle<CR>', { silent = true } },
