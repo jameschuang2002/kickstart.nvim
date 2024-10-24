5c86fada4b716d189e879b9e7afb33735dce6f06
James Chuang <jameschuang2002@gmail.com>
2024-10-24 08:17:28 -0700

    remove < on autoclose

--- init.lua
+++ init.lua
@@ -251,7 +251,7 @@ require('lazy').setup({
     config = function()
       require('autoclose').setup {
         keys = {
-          ['<'] = { escape = true, close = true, pair = '<>', disabled_filetypes = {} },
+          ['<'] = { escape = true, close = false, pair = '<>', disabled_filetypes = {} },
           ['('] = { escape = true, close = true, pair = '()' },
           ['['] = { escape = true, close = true, pair = '[]' },
           ['{'] = { escape = true, close = true, pair = '{}' },
